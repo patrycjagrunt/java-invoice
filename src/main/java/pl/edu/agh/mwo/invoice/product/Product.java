9736eadc13c790eb3dadf9345691e7932a94a76f
Grunt <patrycja.grunt@saint-gobain.com>
2021-01-17 11:22:10 +0100

    ProductTest is working

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -2,6 +2,8 @@ package pl.edu.agh.mwo.invoice.product;
 
 import java.math.BigDecimal;
 
+import org.hamcrest.core.Is;
+
 public abstract class Product {
     private final String name;
 
@@ -10,24 +12,32 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
-        this.name = name;
+        if (name==null || name.equals("")) {
+        	throw new IllegalArgumentException("You cannot create products with null or empty name.");
+        }
+
+        if (price==null || price.signum()==-1) {
+        	throw new IllegalArgumentException("You cannot create products with null or negative price.");
+        }
+        
+    	this.name = name;
         this.price = price;
         this.taxPercent = tax;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return this.price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return this.taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        return this.price.multiply(this.taxPercent).add(price);
     }
 }
