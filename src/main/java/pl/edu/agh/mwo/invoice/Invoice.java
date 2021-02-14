6a5533e4f789d30fb6fdc3f07b3274a70d4e89ca
Patrycja Grunt <grunt@student.agh.edu.pl>
2021-02-14 19:24:59 +0100

    All tests are working

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -2,29 +2,49 @@ package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    
+	private Map<Product,Integer> products = new LinkedHashMap<>();
 
-    public void addProduct(Product product) {
-        // TODO: implement
+	public void addProduct(Product product) {
+        this.products.put(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+    	this.products.put(product, quantity);
+    	 if (quantity==null || quantity<=0) {
+         	throw new IllegalArgumentException("You cannot create products with null or zero quantity.");
+         }
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+    	BigDecimal sum = BigDecimal.ZERO;
+    	for (Product product: this.products.keySet()) {
+    		Integer quantity = this.products.get(product);
+    		sum = sum.add(product.getPrice().multiply(new BigDecimal(quantity)));
+    	} 
+    	
+    	return sum;
     }
 
     public BigDecimal getTax() {
-        return null;
+    	BigDecimal tax = BigDecimal.ZERO;
+    	tax = getTotal().subtract(getSubtotal());
+    	return tax;
     }
 
     public BigDecimal getTotal() {
-        return null;
+    	BigDecimal sum = BigDecimal.ZERO;
+    	for (Product product: this.products.keySet()) {
+    		Integer quantity = this.products.get(product);
+    		sum = sum.add(product.getPriceWithTax().multiply(new BigDecimal(quantity)));
+    	} 
+    	
+    	return sum;
     }
 }
