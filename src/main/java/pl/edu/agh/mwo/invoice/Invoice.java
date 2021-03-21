1112a3769693583b3346cdbd87d286ebe82e23a0
Patrycja Grunt <grunt@student.agh.edu.pl>
2021-03-21 20:24:42 +0100

    tests are working

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -3,21 +3,32 @@ package pl.edu.agh.mwo.invoice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Map<Product, Integer> products = new HashMap<Product, Integer>();
+    private final int invoiceNumber = Math.abs(new Random().nextInt());
+	private Map<Product, Integer> products = new HashMap<Product, Integer>();
 
     public void addProduct(Product product) {
-        addProduct(product, 1);
+    	addProduct(product,1);
     }
-
+ 
     public void addProduct(Product product, Integer quantity) {
         if (product == null || quantity <= 0) {
             throw new IllegalArgumentException();
         }
+        Integer addedProducts = products.get(product);
+        if (addedProducts != null) {
+            quantity = addedProducts + quantity;
+        }
         products.put(product, quantity);
+    }
+    
+    public Map<Product, Integer> getProduct() {
+        return products;
     }
 
     public BigDecimal getNetTotal() {
@@ -41,4 +52,30 @@ public class Invoice {
         }
         return totalGross;
     }
+
+	public int getNumber() {
+		return invoiceNumber;
+	}
+
+	public int getNumberOfProduct() {
+		return  products.size();
+	}
+
+	public boolean PrintInvoice() {
+		
+		System.out.println("Nr. faktury: " + getNumber());
+		System.out.println("Nazwa produktu,   Cena,    Ilość");
+		
+		Map<Product, Integer> products = new HashMap<Product, Integer>();
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+			System.out.printf(entry.getKey().getName(), entry.getValue(), entry.getKey().getPrice());
+        }
+
+        System.out.println("Liczba pozycji: " + products.size());
+        
+		return true;
+	}
+	
+	
+	
 }
--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -34,4 +34,6 @@ public abstract class Product {
     public BigDecimal getPriceWithTax() {
         return price.multiply(taxPercent).add(price);
     }
+
+
 }
